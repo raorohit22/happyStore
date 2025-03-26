eb26d1f95bdd7acd14b9aa5f0116449e85eda4cf
Rohit Yadav <iamrao22@gmail.com>
2025-03-26 16:51:34 +0530

    changes done

--- src/components/invoice/InvoiceForDownload.jsx
+++ src/components/invoice/InvoiceForDownload.jsx
@@ -345,7 +345,7 @@ const InvoiceForDownload = ({
               >
                 <Image
                   src={logoDark}
-                  alt="kachabazar"
+                  alt="happyStore"
                   style={{
                     width: 90,
                     alignItems: "right",
--- src/hooks/useNotification.js
+++ src/hooks/useNotification.js
@@ -4,44 +4,44 @@ import { useDispatch } from "react-redux";
 // import io from "socket.io-client";
 
 const useNotification = () => {
-  const dispatch = useDispatch();
-  const [socket, setSocket] = useState(null);
-  const [updated, setUpdated] = useState(false);
+	const dispatch = useDispatch();
+	const [socket, setSocket] = useState(null);
+	const [updated, setUpdated] = useState(false);
 
-  // useEffect(() => {
-  //   setSocket(io(import.meta.env.VITE_APP_API_SOCKET_URL));
-  //   // setSocket(io("https://kachabazar-backend-theta.vercel.app"));
-  // }, []);
+	// useEffect(() => {
+	//   setSocket(io(import.meta.env.VITE_APP_API_SOCKET_URL));
+	//   // setSocket(io("https://happyStore-backend-theta.vercel.app"));
+	// }, []);
 
-  // useEffect(() => {
-  //   // Listen for the 'notification' event from the server
-  //   socket?.on("notification", (notification) => {
-  //     // Update data in real-time here
-  //     console.log("notification", notification);
-  //     if (notification?.option === "globalSetting") {
-  //       dispatch(removeSetting("globalSetting"));
-  //       const globalSettingData = {
-  //         ...notification.globalSetting,
-  //         name: "globalSetting",
-  //       };
-  //       dispatch(addSetting(globalSettingData));
-  //     }
-  //     // if(notification?.option === 'storeCustomizationSetting'){
+	// useEffect(() => {
+	//   // Listen for the 'notification' event from the server
+	//   socket?.on("notification", (notification) => {
+	//     // Update data in real-time here
+	//     console.log("notification", notification);
+	//     if (notification?.option === "globalSetting") {
+	//       dispatch(removeSetting("globalSetting"));
+	//       const globalSettingData = {
+	//         ...notification.globalSetting,
+	//         name: "globalSetting",
+	//       };
+	//       dispatch(addSetting(globalSettingData));
+	//     }
+	//     // if(notification?.option === 'storeCustomizationSetting'){
 
-  //     // }
-  //   });
+	//     // }
+	//   });
 
-  //   return () => {
-  //     // Disconnect the socket when the component unmounts
-  //     socket?.disconnect();
-  //   };
-  // }, [socket]);
+	//   return () => {
+	//     // Disconnect the socket when the component unmounts
+	//     socket?.disconnect();
+	//   };
+	// }, [socket]);
 
-  return {
-    socket,
-    updated,
-    setUpdated,
-  };
+	return {
+		socket,
+		updated,
+		setUpdated,
+	};
 };
 
 export default useNotification;
