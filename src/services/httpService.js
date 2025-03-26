ac2f7d0b4175a0b0026981f94fffb4f304bcfd0b
Rohit Yadav <iamrao22@gmail.com>
2025-03-26 17:34:38 +0530

    cors fixed

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -4,54 +4,55 @@ const baseUrl = import.meta.env.VITE_APP_API_BASE_URL;
 console.log("base url", baseUrl);
 
 const instance = axios.create({
-  baseURL: `${import.meta.env.VITE_APP_API_BASE_URL}`,
-  timeout: 50000,
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-  },
+	baseURL: `${import.meta.env.VITE_APP_API_BASE_URL}`,
+	timeout: 50000,
+	headers: {
+		Accept: "application/json",
+		"Content-Type": "application/json",
+	},
+	withCredentials: true,
 });
 
 // Add a request interceptor
 instance.interceptors.request.use(function (config) {
-  // Do something before request is sent
-  let adminInfo;
-  if (Cookies.get("adminInfo")) {
-    adminInfo = JSON.parse(Cookies.get("adminInfo"));
-  }
+	// Do something before request is sent
+	let adminInfo;
+	if (Cookies.get("adminInfo")) {
+		adminInfo = JSON.parse(Cookies.get("adminInfo"));
+	}
 
-  let company;
+	let company;
 
-  if (Cookies.get("company")) {
-    company = Cookies.get("company");
-  }
+	if (Cookies.get("company")) {
+		company = Cookies.get("company");
+	}
 
-  // console.log('Admin Http Services Cookie Read : ' + company);
-  // let companyName = JSON.stringify(company);
+	// console.log('Admin Http Services Cookie Read : ' + company);
+	// let companyName = JSON.stringify(company);
 
-  return {
-    ...config,
-    headers: {
-      authorization: adminInfo ? `Bearer ${adminInfo.token}` : null,
-      company: company ? company : null,
-    },
-  };
+	return {
+		...config,
+		headers: {
+			authorization: adminInfo ? `Bearer ${adminInfo.token}` : null,
+			company: company ? company : null,
+		},
+	};
 });
 
 const responseBody = (response) => response.data;
 
 const requests = {
-  get: (url, body, headers) =>
-    instance.get(url, body, headers).then(responseBody),
+	get: (url, body, headers) =>
+		instance.get(url, body, headers).then(responseBody),
 
-  post: (url, body) => instance.post(url, body).then(responseBody),
+	post: (url, body) => instance.post(url, body).then(responseBody),
 
-  put: (url, body, headers) =>
-    instance.put(url, body, headers).then(responseBody),
+	put: (url, body, headers) =>
+		instance.put(url, body, headers).then(responseBody),
 
-  patch: (url, body) => instance.patch(url, body).then(responseBody),
+	patch: (url, body) => instance.patch(url, body).then(responseBody),
 
-  delete: (url, body) => instance.delete(url, body).then(responseBody),
+	delete: (url, body) => instance.delete(url, body).then(responseBody),
 };
 
 export default requests;
